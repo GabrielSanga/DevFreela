9d8b8ae0ea2989750d799e78caad3b598962d5df
GabrielSanga <gabriel.sanga.fef@gmail.com>
2025-12-20 17:49:50 -0300

    Teste

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -18,7 +18,7 @@ namespace DevFreela.API.Controllers
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            return Ok();
+            return Ok("Teste");
         }
 
         [HttpPost]
