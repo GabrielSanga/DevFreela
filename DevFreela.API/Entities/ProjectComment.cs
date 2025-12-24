e10b0830ae7d92cd544c9b8c0862b701893973f3
GabrielSanga <gabriel.sanga.fef@gmail.com>
2025-12-24 15:54:38 -0300

    Implementado dominios- Project/ProjectComments/User/UserSkill/Skill.

---
+++ DevFreela.API/Entities/ProjectComment.cs
@@ -0,0 +1,20 @@
+﻿namespace DevFreela.API.Entities
+{
+    public class ProjectComment : BaseEntity
+    {
+
+        public ProjectComment(string content, int idProject, int idUSer) : base()
+        {
+            Content = content;
+            IdProject = idProject;
+            IdUser = idUSer;
+        }
+
+        public string Content { get; private set; }
+        public int IdProject { get; private set; }
+        public Project Project { get; private set; }
+        public int IdUser { get; private set; }
+        public User User { get; private set; }
+
+    }
+}
