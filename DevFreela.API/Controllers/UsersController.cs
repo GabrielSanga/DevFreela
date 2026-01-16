3df56e7115fcb59354ef73c9ddbfe7802f3d767c
GabrielSanga <gabriel.sanga.fef@gmail.com>
2026-01-16 17:49:41 -0300

    Implementando persistência com banco de dados: User.

--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
-﻿using DevFreela.API.Models;
+﻿using DevFreela.API.Entities;
+using DevFreela.API.Models;
+using DevFreela.API.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.API.Controllers
 {
@@ -9,17 +12,58 @@ namespace DevFreela.API.Controllers
     public class UsersController : ControllerBase
     {
 
-        // POST api/users
+        private readonly DevFreelaDbContext _dbContext;
+
+        public UsersController(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _dbContext.Users
+                            .Include(u => u.Skills)
+                                .ThenInclude(s => s.Skill)
+                            .SingleOrDefault(u => u.Id == id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            var model = UserViewModel.FromEntity(user);
+
+             return Ok(model);
+        }
+
         [HttpPost]
         public IActionResult Post(CreateUserInputModel model)
         {
-            return Ok();
+            var user = model.ToEntity();
+
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+
+            return NoContent();
         }
 
         [HttpPost("{id}/skills")]
-        public IActionResult Post(UserSkillsInputModel model)
+        public IActionResult Post(int id, UserSkillsInputModel model)
         {
-            return Ok();
+            var user = _dbContext.Users.SingleOrDefault(u => u.Id == id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            var userSkills = model.SkillsIds.Select(s => new UserSkill(id, s)).ToList();
+
+            _dbContext.UserSkills.AddRange(userSkills);
+            _dbContext.SaveChanges();
+
+            return NoContent();
         }
 
         [HttpPut("{id}/profile-picture")]
---
+++ DevFreela.API/Models/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DevFreela.API.Entities;
+
+namespace DevFreela.API.Models
+{
+    public class UserViewModel
+    {
+
+        public UserViewModel(int id, string fullName, string email, DateTime birthDate, List<String> skills)
+        {
+            Id = id;
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+            Skills = skills;
+        }
+
+        public int Id { get; private set; }
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public List<String> Skills { get; private set; }
+
+        public static UserViewModel FromEntity(User entity)
+        {
+            return new(entity.Id, entity.FullName, entity.Email, entity.BirthDate, entity.Skills.Select(s => s.Skill.Description).ToList());
+        }
+
+    }
+}
