cf269ed8e3ebe29c50e121921c28a6640ac82e98
GabrielSanga <gabriel.sanga.fef@gmail.com>
2026-01-26 22:35:24 -0300

    Criando Interface e implementação do padrão repository para a entidade Projeto.

--- DevFreela.Application/Queries/GetProjectById/GetProjectByIdQuery.cs
+++ DevFreela.Application/Queries/GetProjectById/GetProjectByIdQuery.cs
@@ -1,7 +1,5 @@
 ﻿using DevFreela.Application.Models;
-using DevFreela.Infrastructure.Persistence;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Application.Queries.GetProjectById
 {
@@ -13,33 +11,5 @@ namespace DevFreela.Application.Queries.GetProjectById
         }
 
         public int Id { get; private set; }
-    }
-
-    public class GetProjectByIdHander : IRequestHandler<GetProjectByIdQuery, ResultViewModel<ProjectViewModel>>
-    {
-        private readonly DevFreelaDbContext _dbContext;
-
-        public GetProjectByIdHander(DevFreelaDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        public async Task<ResultViewModel<ProjectViewModel>> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
-        {
-            var project = await _dbContext.Projects
-                            .Include(p => p.Client)
-                            .Include(p => p.Freelancer)
-                            .Include(p => p.Comments)
-                            .SingleOrDefaultAsync(p => p.Id == request.Id);
-
-            if (project == null)
-            {
-                return ResultViewModel<ProjectViewModel>.Fail("Projeto não encontrado");
-            }
-
-            var model = ProjectViewModel.FromEntity(project);
-
-            return ResultViewModel<ProjectViewModel>.Success(model);
-        }
     }
 }
---
+++ DevFreela.Infrastructure/Persistence/Repositories/ProjectRepository.cs
@@ -0,0 +1,69 @@
+﻿using Azure.Core;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+
+        public ProjectRepository(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddComment(ProjectComment comment)
+        {
+            await _dbContext.ProjectComments.AddAsync(comment);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            return await _dbContext.Projects.AnyAsync(p => p.Id == id);
+        }
+
+        public async Task<List<Project>> GetAll()
+        {
+            var projects = await _dbContext.Projects
+                        .Include(p => p.Client)
+                        .Include(p => p.Freelancer)
+                        .Where(p => !p.IsDeleted)
+                        .ToListAsync();
+
+            return projects;
+        }
+
+        public async Task<Project?> GetById(int id)
+        {
+            return await _dbContext.Projects.SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Project?> GetDetailsById(int id)
+        {
+            var project = await _dbContext.Projects
+                                    .Include(p => p.Client)
+                                    .Include(p => p.Freelancer)
+                                    .Include(p => p.Comments)
+                                    .SingleOrDefaultAsync(p => p.Id == id);
+
+            return project;
+        }
+
+        public async Task<int> Insert(Project project)
+        {
+            await _dbContext.Projects.AddAsync(project);
+            await _dbContext.SaveChangesAsync();
+
+            return project.Id;
+        }
+
+        public async Task Update(Project project)
+        {
+            _dbContext.Projects.Update(project);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
