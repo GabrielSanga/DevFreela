e606b2670297153aface6ee8f974d93bfce7d371
GabrielSanga <gabriel.sanga.fef@gmail.com>
2026-01-29 13:18:06 -0300

    Implementando validações com Fluent Validation para Skill.

---
+++ DevFreela.Application/Validators/InsertSkillValidator.cs
@@ -0,0 +1,22 @@
+﻿using DevFreela.Application.Commands.InserSkill;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Application.Validators
+{
+    public class InsertSkillValidator : AbstractValidator<InsertSkillCommand>
+    {
+
+        public InsertSkillValidator()
+        {
+            RuleFor(s => s.Description)
+                .NotEmpty()
+                    .WithMessage("Nome da skill não pode ser vazio.")
+                .MaximumLength(50)
+                    .WithMessage("Nome da skill deve ter no máximo 50 caracteres.");
+        }   
+
+    }
+}
