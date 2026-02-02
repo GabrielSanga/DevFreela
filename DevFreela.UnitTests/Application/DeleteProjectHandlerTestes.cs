0e10186d57aa65c628270d31a7fd14ae766876ec
GabrielSanga <gabriel.sanga.fef@gmail.com>
2026-02-01 21:39:21 -0300

    Implementado  teste unitário para DeleteProjectHandler com NSubistitute.

--- DevFreela.UnitTests/Application/DeleteProjectHandlerTestes.cs
+++ DevFreela.UnitTests/Application/DeleteProjectHandlerTestes.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DevFreela.Application.Commands.DeleteProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using NSubstitute;
 
 namespace DevFreela.UnitTests.Application
 {
     public class DeleteProjectHandlerTestes
     {
+        [Fact]
+        public async Task ProjectExists_Delete_Success()
+        {
+            //Arrange
+            var project = new Project("Projeto A", "Descrição do projeto A", 1, 2, 1000);
+
+            var repository = Substitute.For<IProjectRepository>();
+            repository.GetById(1).Returns((Project?) project);
+            repository.Update(Arg.Any<Project>()).Returns(Task.CompletedTask);
+            
+            var handler = new DeleteProjectHandler(repository);
+
+            var command = new DeleteProjectCommand(1);
+
+            //Act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            //Assert
+            Assert.True(result.IsSuccess);
+            await repository.Received(1).GetById(1);
+            await repository.Received(1).Update(Arg.Any<Project>());
+        }
+
     }
 }
