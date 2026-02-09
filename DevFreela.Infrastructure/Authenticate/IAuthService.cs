92cef2d292d50e6171b70931c68bd69803bff1f1
GabrielSanga <gabriel.sanga.fef@gmail.com>
2026-02-08 22:30:38 -0300

    Preparando aplicação para serviço de autenticação com JWT.

---
+++ DevFreela.Infrastructure/Authenticate/IAuthService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DevFreela.Infrastructure.Authenticate
+{
+    public interface IAuthService
+    {
+
+        string ComputeHash(string password);
+
+        string GenerateToken(string email, string role); 
+
+    }
+
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string ComputeHash(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateToken(string email, string role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
